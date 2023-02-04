16b7ee2c4b8fceccfddfc03964bda8471ce4235b
Aidan <survivalking4@gmail.com>
2023-02-04 13:41:00 -0700

    rkyfdsz/

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ onfetch = (async function(event) {
     if ((new URL(event.request.url)).search != "?sharing") return;
     //if (event.request.method === "POST") {
         fd = event.request.formData();
-        const file = fd.getAll("images")[0];
+        const file = fd.get("images");
         event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/?recieving#light.aidan_s_room_lights', 303));
         const client = await self.clients.get(event.resultingClientId || event.clientId);
         client.postMessage({file, action:"load-img"});
