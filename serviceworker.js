7418ac0de673cd5c5b944e8c4956f2fb1af42729
Aidan <survivalking4@gmail.com>
2023-02-04 12:49:51 -0700

    gckm fx

--- serviceworker.js
+++ serviceworker.js
@@ -4,7 +4,7 @@ self.addEventListener("fetch", async function(event) {
     if (event.request.method === "POST") {
         var fd = event.request.formData();
         const file = fd.getAll("images")[0];
-        event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/#light.aidan_s_room_lights'));
+        event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/#light.aidan_s_room_lights', 303));
         const client = await self.clients.get(event.resultingClientId || event.clientId);
         client.postMessage({file, action:"load-img"});
     }
