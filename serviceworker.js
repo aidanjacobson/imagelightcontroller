ea999f2aaa073c6cd2ad9dd54f00c2f1faaac091
Aidan <survivalking4@gmail.com>
2023-02-04 13:11:53 -0700

    szfnjm cfry

--- serviceworker.js
+++ serviceworker.js
@@ -3,8 +3,8 @@ onfetch = (async function(event) {
     //if (event.request.method === "POST") {
         var fd = event.request.formData();
         const file = fd.getAll("images")[0];
-        event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/#light.aidan_s_room_lights', 303));
+        /*event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/#light.aidan_s_room_lights', 303));
         const client = await self.clients.get(event.resultingClientId || event.clientId);
-        client.postMessage({file, action:"load-img"});
+        client.postMessage({file, action:"load-img"});*/
     //}
 })
