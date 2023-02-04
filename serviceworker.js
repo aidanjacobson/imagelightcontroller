74f8f2832bdfc8b13675740a2090b18c266d282e
Aidan <survivalking4@gmail.com>
2023-02-04 12:57:04 -0700

    rtshbdz x

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,10 @@
 self.addEventListener("fetch", async function(event) {
-    if (event.request.url.indexOf("light.") == -1) return;
-    const url = new URL(event.request.url);
-    if (event.request.method === "POST") {
+    if (event.request.url.indexOf("lighthandle")) return;
+    //if (event.request.method === "POST") {
         var fd = event.request.formData();
         const file = fd.getAll("images")[0];
         event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/#light.aidan_s_room_lights', 303));
         const client = await self.clients.get(event.resultingClientId || event.clientId);
         client.postMessage({file, action:"load-img"});
-    }
+    //}
 })
