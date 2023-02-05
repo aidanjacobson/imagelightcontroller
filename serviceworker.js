3fb45285f7d8675705adc9a6550e4a8b038e0fec
Aidan <survivalking4@gmail.com>
2023-02-04 22:57:53 -0700

    tersthnb

--- serviceworker.js
+++ serviceworker.js
@@ -3,13 +3,11 @@ onfetch = (async function(event) {
     console.log("fetch");
     if (event.request.url.indexOf("upload-image") == -1) return;
     console.log("upload img");
-    event.respondWith((async ()=>{
-        fd = await event.request.formData();
-        const file = fd.get("images");
-        const client = await self.clients.get(event.resultingClientId || event.clientId);
-        client.postMessage({file, action:"load-img"});
-        return Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/?recieving#light.aidan_s_room_lights', 303);
-    })());
+    event.respondWith(Response.redirect('https://aidanjacobson.github.io/imagelightcontroller/?recieving#light.aidan_s_room_lights', 303));
+    fd = await event.request.formData();
+    const file = fd.get("images");
+    const client = await self.clients.get(event.resultingClientId || event.clientId);
+    client.postMessage({file, action:"load-img"});
 })
 console.log("hi");
 
